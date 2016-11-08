2a349a040ef919024cb3b951eef3d6f6d9de8060
yuanshenli <yuanshenli@DN0a225677.SUNet>
2016-11-08 03:16:44 -0800

    All function

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,7 +4,17 @@
 void ofApp::setup(){
     // This gets called once when we start our app
 
-    // Make the app full screen
+    sound.load("beats.mp3");
+//    sound.load("ninja.mp3");
+    sound.setLoop( true );
+    
+    laughSound.load("laugh.wav");
+    cheerSound.load("cheer.mp3");
+    cheerSound.setMultiPlay(true);
+    bumpSound.load("bump.mp3");
+    
+    
+    myfont.load("arial.ttf", 14, true, false, true, 0.1);
     ofSetFullscreen(false);
     
     // Turn on depth testing so rendering happens according to z-depth rather
@@ -13,20 +23,17 @@ void ofApp::setup(){
     
     // Black background
     ofBackground(0);
-    
-    // Set the size of our sphere "planets"
-    
-    // Initialize orbit rotation angles to zero
 
     windowWidth = ofGetWindowWidth();
     windowHeight = ofGetWindowHeight();
-    starRangeWidth = windowWidth/4;
-    starRangeHeight = windowHeight/4;
+    starRangeWidth = windowWidth/8;
+    starRangeHeight = windowHeight/8;
     
     for(int i = 0; i < starsCount; i++) {
         Star star;
         star.timestamp = 0;
         star.generateLocation(distribution, generator, starRangeWidth, starRangeHeight, initialStarZ * distribution(generator));
+        star.generateColor(distribution, generator);
         star.obj.set(boxSize, boxSize, boxSize);
         stars.push_back(star);
     }
@@ -35,16 +42,107 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
     // This gets called once for each graphical frame, right before draw()
+    sound.setPan(offsetX / starRangeWidth); // pan the sound according to position
+    currentTime = ofGetElapsedTimeMillis(); // read time
+    // Initial waiting screen;
+    if (isInitialState) {
+        timePassed = 0;
+        score = scoreMax;
+        isStart = false;
+        isPause = false;
+        isContinue = false;
+        isHit = false;
+        isGuarded = false;
+        ofBackground(200, 200, 200);
+        
+    }
     
+    if (!isStart) {
+        ofBackground(200, 200, 200);    // pause
+        sound.setPaused(true);
+    } else {
+        sound.setPaused(false);
+//        sound.play();
+        ofBackground(0, 0, 0);          // play
+        timePassed += currentTime - lastTime;   // Update timer if the game is running
+    }
+    if (score <= 0) {
+        finalTimeLeft = gameTime - timePassed/1000;
+        isInitialState = true;
+        isLose = true;
+        laughSound.play();
+    }
+    if (timePassed > (gameTime + 1) * 1000) {
+        isInitialState = true;
+        isWin = true;
+        cheerSound.play();
+    }
+    // Update the background color when a start is hit
+    if (isStart && isHit) {
+        ofBackground(255 - 10*hitCount, 255 - 10*hitCount, 255 - 10*hitCount);
+        hitCount++;
+        if (hitCount == hitCountMax) {
+            hitCount = 0;
+            isHit = false;
+        }
+    }
+    // Avoid hitting multiple stars at the same time.
+    if (isGuarded) {
+        guardCount--;
+        if (guardCount == 0) {
+            guardCount = guardCountMax;
+            isGuarded = false;
+        }
+    }
+    // Check individual stars
     for(Star& star: stars) {
         star.location.z -= starSpeed;
+        // check if hit the stars
+        float distance2 = star.location.z * star.location.z
+        + (star.location.x+offsetX) * (star.location.x+offsetX)
+        + (star.location.y+offsetY) * (star.location.y+offsetY);
+        if (isStart && (distance2 < 200) ) {
+            isHit = true;
+            bumpSound.play();
+            if (!isGuarded){
+                score -= 5;
+                finalScoreLeft = score;
+            }
+            isGuarded = true;
+        }
+        // Check if the start is behind
         if (star.location.z < 0) {
             star.generateLocation(distribution, generator, starRangeWidth, starRangeHeight, initialStarZ);
             star.timestamp = ofGetFrameNum();
         }
-        
-        cout<<star.location.x << ", "<<star.location.y << ", "<< star.location.z << std::endl;
     }
+    
+    // check key press
+    if (isStart) {
+        if (directionLeft){
+            if (offsetX < starRangeWidth * 0.95){
+                offsetX += moveSpeed;
+            }
+        }
+        if (directionRight){
+            if (offsetX > -starRangeWidth * 0.95){
+                offsetX -= moveSpeed;
+            }
+        }
+        if (directionUp){
+            if (offsetY > -starRangeHeight * 0.75){
+                offsetY -= moveSpeed;
+            }
+        }
+        if (directionDown){
+            if (offsetY < starRangeHeight * 0.75){
+                offsetY += moveSpeed;
+            }
+        }
+    }
+      // Update Timer
+    lastTime = currentTime;
+    
 }
 
 //--------------------------------------------------------------
@@ -57,50 +155,195 @@ void ofApp::draw(){
     cam.setFov(100);
     cam.setNearClip(1);
     cam.setFarClip(20000);
+    // Draw stars
     ofPushMatrix(); // Save our state for later
+    
     {
         float centerX = offsetX;
         float centerY = offsetY;
         float currentFrame = ofGetFrameNum();
         // Since our coordinates start in the top left, translate to the center.
         ofTranslate( centerX, centerY, 0);
-        
-        ofSetColor(255, 255, 255);
-        
+        // tilt the trajectory
+        k1 = ofSignedNoise(1,currentFrame/100);
+        k2 = ofSignedNoise(currentFrame/100,1);
+        ofPolyline line1;
+        ofPolyline line2;
+        ofPolyline line3;
+        ofPolyline line4;
+        for (int z = 0; z < 100; z++) {
+            int trueZ = z * 10;
+            int x1 = -140 + k1 * trueZ * trueZ/scaleDis;
+            int x2 = 140 + k1 * trueZ * trueZ/scaleDis;
+            int y1 = -140 + k2 * trueZ * trueZ/scaleDis;
+            int y2 = 140 + k2 * trueZ * trueZ/scaleDis;
+            line1.addVertex(x1, y1, -trueZ);
+            line2.addVertex(x2, y1, -trueZ);
+            line3.addVertex(x1, y2, -trueZ);
+            line4.addVertex(x2, y2, -trueZ);
+            if (z % 20 == 20 - (int)floor(currentFrame) % 20) {
+                ofDrawLine(x1, y1, -trueZ, x2, y1, -trueZ);
+                ofDrawLine(x2, y1, -trueZ, x2, y2, -trueZ);
+                ofDrawLine(x2, y2, -trueZ, x1, y2, -trueZ);
+                ofDrawLine(x1, y2, -trueZ, x1, y1, -trueZ);
+            }
+        }
+        line1.draw();
+        line2.draw();
+        line3.draw();
+        line4.draw();
         for (Star& star : stars) {
             ofPushMatrix();
             {
-                ofTranslate(star.location.x, star.location.y, - star.location.z);
+                float zScaled = (star.location.z * star.location.z)/ scaleDis;
+                ofSetColor(star.color.x, star.color.y, star.color.z);
+                ofTranslate(star.location.x + k1 * zScaled, star.location.y+ k2 * zScaled, - star.location.z);
                 star.obj.draw();
-//                moon.drawWireframe();
             }
             ofPopMatrix();
         }
     }
-    ofPopMatrix(); // Back to initial state at the top-left of our window.
+    ofPopMatrix();
+    
+    // Print score
+    ofPushMatrix();
+    {
+//        float currentFrame = ofGetFrameNum();
+        ofTranslate( -windowWidth / 4, windowHeight / 4-10, -180);
+        ofSetColor(255,255,255);
+        myfont.drawStringAsShapes("Your Score:", 0, 0);
+        myfont.drawStringAsShapes(std::to_string(score), 100, 0);
+    }
+    ofPopMatrix();
+    // Print timer
+    ofPushMatrix();
+    {
+        int timeLeft = gameTime - timePassed/1000;
+//        float currentFrame = ofGetFrameNum();
+        ofTranslate( windowWidth / 4-100, windowHeight / 4-10, -180);
+        ofSetColor(255,255,255);
+        myfont.drawStringAsShapes("Time left:", 0, 0);
+        myfont.drawStringAsShapes(std::to_string(timeLeft), 90, 0);
+        
+    }
+    ofPopMatrix();
+    // Print instructions
+    ofPushMatrix();
+    {
+//        float currentFrame = ofGetFrameNum();
+        
+        ofSetColor(255,255,0);
+        if (!isStart && !isWin && !isLose) {
+            if (!isPause) {
+                ofTranslate(-140, 0, -180);
+                myfont.drawStringAsShapes("Press [s] to start, Press [p] to pause", 0, 0);
+                myfont.drawStringAsShapes("[1]: Easy", -windowWidth / 5 + 80, -windowWidth / 5 + 40);
+                myfont.drawStringAsShapes("[2]: Hard", -windowWidth / 5 + 80, -windowWidth / 5 + 10);
+            } else {
+                ofTranslate(-70, 0, -180);
+                myfont.drawStringAsShapes("Press [s] to resume", 0, 0);
+            }
+        } else if (isWin) {
+            ofTranslate(-70, 0, -180);
+            myfont.drawStringAsShapes("YOU ARE AWESOME!", 0, 20);
+            myfont.drawStringAsShapes("Your score: ", 20, -20);
+            myfont.drawStringAsShapes(std::to_string(finalScoreLeft), 140, -20);
+        } else if (isLose) {
+            ofTranslate(-80, 0, -180);
+            myfont.drawStringAsShapes("YOU CAN DO BETTER!", 0, 20);
+            myfont.drawStringAsShapes("Time left: ", 20, -20);
+            myfont.drawStringAsShapes(std::to_string(finalTimeLeft), 140, -20);
+        }
+
+    }
+    ofPopMatrix();
+    ofPushMatrix();
+    {
+        if (isInitialState) {
+            ofTranslate( 0, 0, 0);
+            ofSetColor(255,255,0);
+            myfont.drawStringAsShapes("[1]: Hard", 0, 20);
+            myfont.drawStringAsShapes("[2]: Harder", 0, 0);
+        }
+    }
+    ofPopMatrix();
+    
     cam.end();
     
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    if (key == '1'){
+        if (isInitialState){
+            sound.unload();
+            sound.load("beats.mp3");
+            gameTime = 60;
+        }
+        
+    }
+    if (key == '2'){
+        if (isInitialState){
+            sound.unload();
+            sound.load("ninja.mp3");
+            gameTime = 155;
+        }
+        
+    }
+    if (key == 's') {
+        if (isPause) {
+            isPause = false;
+            isContinue = true;
+        } else {
+            // the first time to start
+            isContinue = false;
+            ofGetElapsedTimeMillis();
+            isWin = false;
+            isLose = false;
+            if (!isStart){
+                sound.play();
+            }
+        }
+        isStart = true;
+        isInitialState = false;
+    }
+    if (key == 'p') {
+        isStart = false;
+        isPause = true;
+    }
+    if (key == 'r') {
+        isInitialState = true;
+        isWin = false;
+        isLose = false;
+    }
     if (key == OF_KEY_LEFT){
-        offsetX -= moveSpeed;
+        directionLeft = true;
     }
     if (key == OF_KEY_RIGHT){
-        offsetX += moveSpeed;
+        directionRight = true;
     }
     if (key == OF_KEY_UP){
-        offsetY += moveSpeed;
+        directionUp = true;
     }
     if (key == OF_KEY_DOWN){
-        offsetY -= moveSpeed;
+        directionDown = true;
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    if (key == OF_KEY_LEFT){
+        directionLeft = false;
+    }
+    if (key == OF_KEY_RIGHT){
+        directionRight = false;
+    }
+    if (key == OF_KEY_UP){
+        directionUp = false;
+    }
+    if (key == OF_KEY_DOWN){
+        directionDown = false;
+    }
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -19,6 +19,12 @@ class ofApp : public ofBaseApp{
             location.y = 2 * distribution(generator) * height - height;
             location.z = depth;
         }
+        ofVec3f color;
+        void generateColor(std::uniform_real_distribution<float> & distribution, std::default_random_engine & generator) {
+            color.x = distribution(generator) * 255;
+            color.y = distribution(generator) * 255;
+            color.z = distribution(generator) * 255;
+        }
     };
     const float initialStarZ = 1000;
     const float starSpeed = 10;
@@ -29,6 +35,43 @@ class ofApp : public ofBaseApp{
     std::vector<Star> stars;
     std::default_random_engine generator;
     std::uniform_real_distribution<float> distribution;
+    bool directionUp = false;
+    bool directionDown = false;
+    bool directionLeft = false;
+    bool directionRight = false;
+    // guard parameter
+    bool isGuarded = false;
+    const int guardCountMax = 50;
+    int guardCount = guardCountMax;
+    // hit parameter
+    bool isHit = false;
+    const int hitCountMax = 25;
+    int hitCount = 0;
+    const int scoreMax = 100;
+    int score;
+    
+    // game states
+    bool isStart = false;
+    bool isPause = false;
+    bool isContinue = false;
+    bool isInitialState = true;
+    bool isWin = false;
+    bool isLose = false;
+    int finalTimeLeft = 0;
+    int finalScoreLeft = 0;
+    int gameTime = 60;
+    
+    // time parameters
+    int lastTime = 0;
+    int currentTime = 0;
+    int timePassed = 0;
+    // displacement
+    float k1,k2;
+    int scaleDis = 3000;
+    
+    
+    ofTrueTypeFont myfont;
+    
 
 public:
     void setup();
@@ -48,6 +91,9 @@ public:
     void gotMessage(ofMessage msg);
 
     // OpenFrameworks has many handy graphical primitive objects
-    
+    ofSoundPlayer sound;
+    ofSoundPlayer laughSound;
+    ofSoundPlayer cheerSound;
+    ofSoundPlayer bumpSound;
     ofCamera cam;
 };
